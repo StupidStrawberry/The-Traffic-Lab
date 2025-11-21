c5a5877c95da12d328fc742a84cad9c62e2c33f7
TolyaHuligan <12345ghfuf@gmail.com>
2025-11-22 01:05:40 +0700

    ПЕРЕКРЕСТОК

--- classes.py
+++ classes.py
@@ -1,17 +1,20 @@
 class Vehicle:
-    def __init__(self, id, type):
+    def __init__(self, id, type, direction):
         self.id = id
         self.type = type
+        self.direction = direction  # 'horizontal' или 'vertical'
 
 class Pedestrian:
-    def __init__(self, id):
+    def __init__(self, id, direction):
         self.id = id
+        self.direction = direction  # 'horizontal' или 'vertical'
 
 class Crosswalk:
-    def __init__(self, id, position, width):
+    def __init__(self, id, position, width, direction):
         self.id = id
         self.position = position
         self.width = width
+        self.direction = direction
         self.pedestrians = []
 
     def add_pedestrian(self, pedestrian):
